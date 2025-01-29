0a8b30479a046da871b7b978f343951c660671eb
Ayan <ayanvahmad6392@gmail.com>
2025-01-29 15:54:40 +0530

    literal folder got commited

---
+++ 1-literals/Main1.java
@@ -0,0 +1,11 @@
+class Main1{
+    public static void main(String[] args){
+        // literals are the value which are stored in variable.
+        int num = 0b101; // Here we are storing binary value in num
+        System.out.println(num);
+        int num2 = 10_00_00_000; //Here underscore help in counting number withot error
+        System.out.println(num2);
+        double num3 = 12e10; // 12^10
+        System.out.println(num3);
+    }
+}
